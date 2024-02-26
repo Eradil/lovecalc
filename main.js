843ac125f3113404e6f3f5eab3dae985619ef914
Eradil <eradil.abasbekov.ss@gmail.comgit config --global user.name Eradil git config --global user.name Eradil git config --global user.email eradil.abasbekov.ss@gmail.com>
2024-02-27 00:08:08 +0300

    feat: add some animation just example

--- main.js
+++ main.js
@@ -1,32 +1,3 @@
-// function checkloveper() {
-//   var name = document.getElementById("name").value;
-//   var lname = document.getElementById("lname").value;
-
-//   if (name == "") {
-//     alert("Атыңызды жазыңыз");
-//   } else if (name.length <= 2) {
-//     alert("жок дегенде 3 тамга жазуу керек");
-//   } else if (!isNaN(name)) {
-//     alert("сандар ат болуп эсептелбейт");
-//   } else if (lname == "") {
-//     alert("Сүйгөнүңүздүн атын жазыңыз");
-//   } else if (lname.length <= 2) {
-//     alert("жок дегенде 3 тамга жазуу керек");
-//   } else if (!isNaN(lname)) {
-//     alert("сандар ат болуп эсептелбейт");
-//   } else {
-//     var lovdata = Math.random() * 100;
-//     lovdata = Math.floor(lovdata);
-//     document.getElementById("lovevalue").value = lovdata + "%";
-//   }
-//   if (lovdata > 70) {
-//     alert(`Той качан? сиздин шайкештигиңиз ${lovdata}% барабар♥`);
-//   } else if (lovdata > 50 && lovdata < 70) {
-//     alert(`Жакшы жанылык! Cиздин шайкештигиңиз ${lovdata}% барабар♥`);
-//   } else {
-//     alert(`Cиздин шайкештигиңиз ${lovdata}% барабар♥`);
-//   }
-// }
 function checkloveper() {
   var name = document.getElementById("name").value;
   var lname = document.getElementById("lname").value;
@@ -69,5 +40,6 @@ function checkloveper() {
     } else {
       alert(`Cиздин шайкештигиңиз ${lovdata}% барабар♥`);
     }
+    // Вывод сообщения в зависимости от уровня "любви"
   }
 }
